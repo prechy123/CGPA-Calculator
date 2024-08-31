821af7be3a1c2256a7c1642ef6bfb9f473e1388e
prechy123 <bamideleprecious100@gmail.com>
2024-08-31 22:20:19 +0100

    added asyncStorage

---
+++ utils/storage.js
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem("my-key", jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+export const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem("my-key");
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
